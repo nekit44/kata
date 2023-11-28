db889bac0e5f6333c805eaf792165394f4771de9
Nikita <nekit81@ya.ru>
2023-11-28 14:37:32 +0300

    MergeTwoSortedLists leetcode

--- src/Easy/Array/MergeTwoSortedLists.php
+++ src/Easy/Array/MergeTwoSortedLists.php
@@ -3,16 +3,30 @@ declare(strict_types=1);
 
 namespace App\Easy\Array;
 
+use App\Common\ListNode;
+
 class MergeTwoSortedLists
 {
-    public function __invoke(array $list1, array $list2): array
+    /**
+     * https://leetcode.com/problems/merge-two-sorted-lists/solutions/3356849/clean-solution-in-php-no-recursion/
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return array
+     */
+    public function __invoke(?ListNode $list1, ?ListNode $list2): ?ListNode
     {
-        $result = [];
-        array_map(function ($a, $b) use (&$result) {
-           if ($a !== null) $result[] = $a;
-           if ($b !== null) $result[] = $b;
-        }, $list1, $list2);
-        asort($result);
-        return $result;
+        $head = $tail = new ListNode();
+        while (isset($list1, $list2)) {
+            if ($list1->val < $list2->val) {
+                $tail->next = $list1;
+                $list1 = $list1->next;
+            } else {
+                $tail->next = $list2;
+                $list2 = $list2->next;
+            }
+            $tail = $tail->next;
+        }
+        $tail->next = $list1 ?? $list2;
+        return $head->next;
     }
 }
--- tests/easy/Array/MergeTwoSortedListsTest.php
+++ tests/easy/Array/MergeTwoSortedListsTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace easy\Array;
 
+use App\Common\ListNode;
 use App\Easy\Array\MergeTwoSortedLists;
 use PHPUnit\Framework\TestCase;
 
@@ -10,8 +11,7 @@ class MergeTwoSortedListsTest extends TestCase
 {
     public function testInvoke()
     {
-        self::assertEquals([1,1,2,3,4,4], (new MergeTwoSortedLists())([1,2,4], [1,3,4]));
-        self::assertEquals([], (new MergeTwoSortedLists())([], []));
-        self::assertEquals([0], (new MergeTwoSortedLists())([], [0]));
+        $test = (new MergeTwoSortedLists())(new ListNode([1,2,4]), new ListNode([1,3,4]));
+        self::assertTrue($test instanceof ListNode);
     }
 }
